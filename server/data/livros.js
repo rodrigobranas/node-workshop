91efcd8fd46b0ea1eb1f149cb26ae2255792aa5a
Rodrigo Branas <rodrigo.branas@gmail.com>
2016-05-14 11:05:14 -0300

    updated

--- server/data/livros.js
+++ server/data/livros.js
@@ -1,20 +1,28 @@
 var Livro = require('../domain/livro');
 var fs = require('fs');
+var zlib = require('zlib');
 
 var livros = [];
 
 var carregarLivros = function () {
-	fs.readFile(__dirname + '/livros.csv', 'utf8', function (err, data) {
+	fs.readFile(__dirname + '/livros.zip', function (err, zip) {
 		if (err) {
 			console.log(err);
 			return;
 		}
-		var linhas = data.split('\n');
-		linhas.forEach(function (linha) {
-			var propriedades = linha.split(';');
-			var livro = Livro.fromProperties(propriedades);
-			livros.push(livro);
-		})
+		zlib.unzip(zip, function (err, data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var csv = data.toString('utf8');
+			var linhas = csv.split('\n');
+			linhas.forEach(function (linha) {
+				var propriedades = linha.split(';');
+				var livro = Livro.fromProperties(propriedades);
+				livros.push(livro);
+			});
+		});
 	});
 };
 
